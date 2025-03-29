9c77002395a3e6c29eb62b87a8449d7a20e4ef41
Priyasnsh Jain <priyansh01jain@gmail.com>
2025-03-29 12:32:20 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 docker build -t $JOB_NAME:$BUILD_ID .
 
-docker tag $JOB_NAME:$BUILD_ID jacksneel/$JOB_NAME:$BUILD_ID
+docker tag $JOB_NAME:$BUILD_ID priyansh01jain/$JOB_NAME:$BUILD_ID
 
-docker tag $JOB_NAME:$BUILD_ID jacksneel/$JOB_NAME:latest
+docker tag $JOB_NAME:$BUILD_ID priyansh01jain/$JOB_NAME:latest
 
-docker push jacksneel/$JOB_NAME:$BUILD_ID
+docker push priyansh01jain/$JOB_NAME:$BUILD_ID
 
-docker push jacksneel/$JOB_NAME:latest
+docker push priyansh01jain/$JOB_NAME:latest
 
-docker rmi -f $JOB_NAME:$BUILD_ID jacksneel/$JOB_NAME:$BUILD_ID jacksneel/$JOB_NAME:latest
+docker rmi -f $JOB_NAME:$BUILD_ID priyansh01jain/$JOB_NAME:$BUILD_ID priyansh01jain/$JOB_NAME:latest
